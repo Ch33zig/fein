3fe987dfb1e0d413842ac3f63d9bd7af0352b970
Alex Wu <ch33zig@gmail.com>
2025-03-11 16:19:55 -0400

    Update FEINFEINFEIN.py

--- fodler test/FEINFEINFEIN.py
+++ fodler test/FEINFEINFEIN.py
@@ -7,7 +7,7 @@ async def function(args: Dict[str, Any]) -> Dict[str, Any]:
     random numer between 1 and 100
     """
     print("those who know")
-    return {"message": random.randint(1, 100)}
+    return json.dumps({"message": random.randint(1, 100)})
 
 object = {
     "name": "random number",
